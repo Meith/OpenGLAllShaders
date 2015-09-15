0f06ae211e208022f80647db15863e90c294db1d
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-09-15 14:49:53 -0400

    Color attributes passed successfully.

--- Practise/Shaders/tessalation_evaluation_shader.glsl
+++ Practise/Shaders/tessalation_evaluation_shader.glsl
@@ -2,12 +2,12 @@
 
 layout (triangles, equal_spacing, cw) in;
 
-patch in vec4 tcs_color;
+in vec4 tcs_color[];
 
 out vec4 tes_color;
 
 void main()
 { 
 	gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position) + (gl_TessCoord.y * gl_in[1].gl_Position) + (gl_TessCoord.z * gl_in[2].gl_Position);
-	tes_color = tcs_color;
+	tes_color = (gl_TessCoord.x * tcs_color[0]) + (gl_TessCoord.y * tcs_color[1]) + (gl_TessCoord.z * tcs_color[2]);
 }          
