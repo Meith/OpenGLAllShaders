43341616726ae4056d796f1a7eb9f248d6e45b2c
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-09-16 09:45:41 -0400

    no message

--- Practise/Shaders/geometry_shader.glsl
+++ Practise/Shaders/geometry_shader.glsl
@@ -1,7 +1,7 @@
 #version 440 core
 
 layout (triangles) in;
-layout (triangle_strip, max_vertices = 3) out;
+layout (triangle_strip, max_vertices = 6) out;
 
 in vec4 tes_color[];
 
@@ -14,6 +14,15 @@ void main()
 	{
 		gs_color = tes_color[i];
 		gl_Position = gl_in[i].gl_Position;
+		EmitVertex();
+	}
+
+	EndPrimitive();
+
+	for(i = 0; i < gl_in.length(); ++i)
+	{
+		gs_color = tes_color[i];
+		gl_Position = gl_in[i].gl_Position + 1.0;
 		EmitVertex();
 	}
 
