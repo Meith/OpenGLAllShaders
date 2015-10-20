9ce12c837ff1c684426b0580d3dc53a680074b25
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-10-20 19:32:20 -0400

    Added two sse functions.

--- Practise/Vector.h
+++ Practise/Vector.h
@@ -5,6 +5,7 @@
 
 #include <math.h>
 #include <xmmintrin.h>
+#include <emmintrin.h>
 
 // operation within vector
 #define vec_in_operation(r, a, n, o) { GLuint i; for(i = 0; i < n; ++i) r = r o a[i]; }
@@ -35,15 +36,50 @@ static _inline void vec##n##_norm(vec##n r, vec##n const a) \
 	vec_scl_operation(r, a, b, n, *); \
 }
 
-vec(2)
-vec(3)
-vec(4)
+vec(2) // x y
+vec(3) // x y z
+vec(4) // x y z w
 
 static _inline void vec3_crossprod(vec3 r, vec3 const a, vec3 const b)
 {
 	r[0] = a[1] * b[2] - a[2] * b[1];
 	r[1] = a[2] * b[0] - a[0] * b[2];
 	r[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+typedef GLfloat bivec4[6]; // xy xz xw yz yw zw
+
+static _inline void vec4_wedgeprod(bivec4 r, vec4 const a, vec4 const b)
+{
+	GLuint i;
+	GLuint j;
+	GLuint k = 0;
+
+	for (i = 0; i < 3; ++i)
+		for (j = i; j < 3; ++j)
+			r[k++] = a[i] * b[j + 1] - a[j + 1] * b[i];
+}
+
+static _inline void vec4_wedgeprod_sse(bivec4 r, vec4 const a, vec4 const b)
+{
+	__declspec (align(16)) __m128 a_cur, b_cur, a_line, b_line, r_line[3];
+
+	GLuint i;
+	GLuint j;
+	GLuint k = 0;
+
+	a_line = _mm_load_ps(a);
+	b_line = _mm_load_ps(b);
+
+	for (i = 0; i < 3; ++i)
+	{
+		a_cur = _mm_set1_ps(a[i]);
+		b_cur = _mm_set1_ps(b[i]);
+
+		r_line[i] = _mm_sub_ps(_mm_mul_ps(a_cur, b_line), _mm_mul_ps(b_cur, a_line));
+		for (j = i + 1; j < 4; ++j)
+			r[k++] = r_line[i].m128_f32[j];
+	}
 }
 
 typedef GLfloat mat4x4[16];
@@ -59,7 +95,7 @@ static _inline void mat4x4_mul(mat4x4 r, mat4x4 const a, mat4x4 const b)
 
 static _inline void mat4x4_mul_sse(mat4x4 r, mat4x4 const a, mat4x4 const b)
 {
-	__m128 a_line, b_line, r_line;
+	__declspec (align(16)) __m128 a_line, b_line, r_line;
 
 	GLuint i, j;
 
