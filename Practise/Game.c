5edcd81ccb10c99f80c748f16aa7f4e7d6c50178
Meith Jhaveri <mj9617@g.rit.edu>
2015-10-20 08:43:55 -0400

    matrix sse multiplication working.

--- Practise/Game.c
+++ Practise/Game.c
@@ -7,6 +7,35 @@
 void Game_Init()
 {
 	InputHandler_Init();
+
+	mat4x4 a = { 
+		4.0f, 3.0f, 2.0f, 1.0f,
+		8.0f, 9.0f, 7.0f, 5.0f,
+		6.0f, 3.0f, 7.0f, 1.0f,
+		3.0f, 2.0f, 9.0f, 4.0f 
+	};
+
+	mat4x4 b = {
+		9.0f, 1.0f, 4.0f, 7.0f,
+		2.0f, 5.0f, 6.0f, 8.0f,
+		3.0f, 1.0f, 9.0f, 4.0f,
+		2.0f, 5.0f, 7.0f, 8.0f
+	};
+
+	mat4x4 r;
+	mat4x4 r_sse;
+
+	mat4x4_mul(r, a, b);
+	mat4x4_mul_sse(r_sse, a, b);
+
+	GLuint i;
+	for (i = 0; i < 16; i += 4)
+		printf("%f %f %f %f\n", r[i], r[i + 1], r[i + 2], r[i + 3]);
+
+	printf("\n");
+
+	for (i = 0; i < 16; i += 4)
+		printf("%f %f %f %f\n", r[i], r[i + 1], r[i + 2], r[i + 3]);
 }
 
 GLuint Game_HandleInput()
--- Practise/Vector.h
+++ Practise/Vector.h
@@ -4,6 +4,7 @@
 #include <GL/glew.h>
 
 #include <math.h>
+#include <xmmintrin.h>
 
 // operation within vector
 #define vec_in_operation(r, a, n, o) { GLuint i; for(i = 0; i < n; ++i) r = r o a[i]; }
@@ -37,7 +38,6 @@ static _inline void vec##n##_norm(vec##n r, vec##n const a) \
 vec(2)
 vec(3)
 vec(4)
-vec(16)
 
 static _inline void vec3_crossprod(vec3 r, vec3 const a, vec3 const b)
 {
@@ -46,15 +46,40 @@ static _inline void vec3_crossprod(vec3 r, vec3 const a, vec3 const b)
 	r[2] = a[0] * b[1] - a[1] * b[0];
 }
 
-typedef vec16 mat4x4;
+typedef GLfloat mat4x4[16];
 
-static _inline void mat4x4_mul(mat4x4 r, mat4x4 a, mat4x4 b)
+static _inline void mat4x4_mul(mat4x4 r, mat4x4 const a, mat4x4 const b)
 {
 	GLuint i, j;
 
-	for (i = 0; i < 4; i += 4)
+	for (i = 0; i < 16; i += 4)
 		for (j = 0; j < 4; ++j)
 			r[i + j] = a[i] * b[j] + a[i + 1] * b[j + 4] + a[i + 2] * b[j + 8] + a[i + 3] * b[j + 12];
+}
+
+static _inline void mat4x4_mul_sse(mat4x4 r, mat4x4 const a, mat4x4 const b)
+{
+	__m128 a_line, b_line, r_line;
+
+	GLuint i, j;
+
+	for (i = 0; i < 16; i += 4)
+	{
+		a_line = _mm_set1_ps(a[i]);
+		b_line = _mm_load_ps(b);
+
+		r_line = _mm_mul_ps(a_line, b_line);
+
+		for (j = 1; j < 4; ++j)
+		{
+			a_line = _mm_set1_ps(a[i + j]);
+			b_line = _mm_load_ps(&b[j * 4]);
+
+			r_line = _mm_add_ps(_mm_mul_ps(a_line, b_line), r_line);
+		}
+
+		_mm_store_ps(&r[i], r_line);
+	}
 }
 
 #endif
